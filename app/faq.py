6abdaf2323a82f125d513d9af0490b981b38942a
Syed Rabbani <rabbanimunna12@gmail.com>
2025-03-13 00:34:51 +0530

    Add files

--- app/faq.py
+++ app/faq.py
@@ -23,7 +23,7 @@ ef = embedding_functions.SentenceTransformerEmbeddingFunction(
 
 
 def ingest_faq_data(path):
-    if collection_name_faq not in [collection_name for collection_name in chroma_client.list_collections()]:
+    if collection_name_faq not in [collection_name for collection_name in chroma_client.list_collections() chromadb.api.client.SharedSystemClient.clear_system_cache()]:
         print("Ingesting FAQ data into Chromadb...")
         collection = chroma_client.get_or_create_collection(
             name=collection_name_faq,
